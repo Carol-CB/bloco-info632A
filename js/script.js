09ccb52c6dbdce0fa164735976517db06496a9df
Carol-CB <carolina-cbertuol@educar.rs.gov.br>
2024-04-01 11:36:14 -0300

    Terceiro commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,109 @@
 let addNote = document.querySelector('#add-note');//Botão de para adicionar nota
-let closeModal =  document.querySelector('#close-modal'); //fechar janela modal com os detalhes da nota.
+let closeModalView =  document.querySelector('#close-modal'); //fechar janela modal com os detalhes da nota.
 let modal = document.querySelector('#modal'); //Modal para edição das notas
 let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
-let notes = document.querySelectorAll('.item-note');//Lista divs com dados das notas
+let notes = document.querySelector('#notes');//Lista divs com dados das notas
 let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
-let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
+let btnCloseModal = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
 
-/**
- * Modal de edição da anotação
- */
-addNote.addEventListener('click',
-(evt)=>{
-  evt.preventDefault();
+//++++++++++++++++EVENTOS+++++++++++++++++++
+
+addNote.addEventListener('click',(evt)=>
+{
+  evt.preventDefault(); //PREVINE DA PÁGINA RECARREGAR COM O LINK
   modal.style.display='block';
-  document.querySelector('#notes').style.display='none';
-  document.querySelector('#controls').style.display='none';
+  notes.style.display = 'none';
+  addNote.style.display = 'none';
 });
+
+btnCloseModal.addEventListener('click', (evt) =>
+{
+  evt.preventDefault();
+  modal.style.display='none';
+  notes.style.display = 'flex';
+  addNote.style.display = 'block';
+});
+
+btnSaveNote.addEventListener('click', (evt) =>
+{
+  evt.preventDefault();
+  let objNote = {
+    id : document.querySelector("#input-id").value.trim(),
+    title : document.querySelector("#input-title").value.trim(),
+    content : document.querySelector("#input-content").value.trim()
+  };
+
+  console.log(objNote);
+  saveNote(objNote);
+});
+
+
+//+++++++++++++++FUNÇÕES+++++++++++++++++++++
+
+const saveNote = (note) =>{
+  let listNotes = loadNotes();
+
+  if(note.id.length < 1){
+    note.id = new Date().getTime();
+    document.querySelector('#input-id').value = note.id;
+    listNotes.push(note);
+  }else{
+    console.log(note.id);
+    listNotes.forEach((item, i) =>{
+      if(item.id == note.id){
+        listNotes[i] = note;
+      }
+    });
+  }
+  note.lastTime = new Date().getTime();
+
+  console.log(listNotes)
+  listNotes = JSON.stringify(listNotes);
+  localStorage.setItem('notes', listNotes);
+};
+
+const loadNotes = () =>{
+  let listNotes = localStorage.getItem('notes');
+  console.log(listNotes);
+
+  if(!listNotes){
+    listNotes = [];
+  }
+  else{
+    listNotes = JSON.parse(listNotes);
+  }
+
+  return listNotes;
+}
+
+const listNotes = () =>{
+  let listNotes =loadNotes();
+  listNotes.forEach((item)=> {
+    let divCard = document.createElement('div');
+    divCard.className = 'card';
+    divCard.style.width = '18rem';
+    notes.appendChild(divCard);
+
+    let divCardBody = document.createElement('div');
+    divCardBody.className = 'card-body';
+    divCard.appendChild(divCardBody)
+
+    let h1 = document.createElement('h1');
+    h1.innerText = item.title;
+    divCardBody.appendChild(h1);
+
+    let pcontent = document.createElement('p');
+    pcontent.innerText = item.content;
+    divCardBody.appendChild(pcontent);
+
+    let pLastTime = document.createElement('p');
+    pLastTime.innerText = item.lastTime;
+    divCardBody.appendChild(pLastTime);
+
+    divCard.addEventListener('click',(evt)=>{
+        
+    });
+  })
+};
+
+listNotes();
